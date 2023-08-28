26f67c317ddf78bcba2559fd0723b65f4654242a
Sushanthigithub <142594589+Sushanthigithub@users.noreply.github.com>
2023-08-28 09:22:42 +0530

    Update DoubleClick_example.java

--- src/test/java/actionsClass/DoubleClick_example.java
+++ src/test/java/actionsClass/DoubleClick_example.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-
+//This is an Example of double click operation
 public class DoubleClick_example {
 
 	public static void main(String[] args)
